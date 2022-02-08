e89ba63da0a90fa8188a4421b3c4d06fddb7f167
Eric Wan <ewan2@illinois.edu>
2022-02-08 00:46:23 -0600

    added workflow

--- docs/make.jl
+++ docs/make.jl
@@ -7,5 +7,5 @@ using .HeatTransfer
 makedocs(sitename = "Numerical Heat Transfer Documentation")
 
 deploydocs(
-    repo = "github.com/arturofburgos/Julia_Repo_Studies.git"
+    repo = "github.com/ClayMasta/Numerical_Heat_Transfer.git"
 )
